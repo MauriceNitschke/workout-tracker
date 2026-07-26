17d1ef808f58229d522d78299d02061d729b63ce
MauriceNitschke <40233071+MauriceNitschke@users.noreply.github.com>
2026-07-26 20:46:29 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: "/workout-tracker/",
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
